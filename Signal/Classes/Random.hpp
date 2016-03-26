b044cdb6bd8974173e711bc84e9d007fce2c4f6a
佐藤 匠 <biker-duck.g@ezweb.ne.jp>
2016-03-27 01:28:44 +0900

    範囲乱数生成関数を定義

---
+++ Signal/Classes/Random.hpp
@@ -0,0 +1,38 @@
+//
+//  Header.hpp
+//  CocosTemplate
+//
+//  Created by 佐藤 匠 on 2016/03/26.
+//
+//
+
+#include
+
+
+#pragma once
+
+#include <stdio.h>
+#include <iostream>
+
+namespace Random {
+    
+    static void Init()
+    {
+        
+        srand((unsigned int)time(NULL));
+        
+    }
+    
+    
+    //範囲乱数生成
+    static float GetRandomRange(float min,float max)
+    {
+        return min + (float)(rand()/ (float)(RAND_MAX/ (max - min)));
+    }
+    static int GetRandomRange(int min,int max)
+    {
+        return min + (int)(rand() * (max-min+1.0)/(1.0+RAND_MAX));
+    }
+
+    }
+}
