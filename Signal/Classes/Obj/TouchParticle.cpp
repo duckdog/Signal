99030ea0389faa068cbe5b0f5e5fd13e410e0e5d
佐藤 匠 <biker-duck.g@ezweb.ne.jp>
2016-03-31 15:53:30 +0900

    タッチ時のパーティクル。動作テスト済み.多分使いやすい...はず

---
+++ Signal/Classes/Obj/TouchParticle.cpp
@@ -0,0 +1,9 @@
+//
+//  TouchParticle.cpp
+//  CocosTemplate
+//
+//  Created by 佐藤 匠 on 2016/03/31.
+//
+//
+
+#include "TouchParticle.hpp"
---
+++ Signal/Classes/Obj/TouchParticle.hpp
@@ -0,0 +1,14 @@
+//
+//  TouchParticle.hpp
+//  CocosTemplate
+//
+//  Created by 佐藤 匠 on 2016/03/31.
+//
+//
+
+#ifndef TouchParticle_hpp
+#define TouchParticle_hpp
+
+#include <stdio.h>
+
+#endif /* TouchParticle_hpp */
