c121e49c6d3cdaea9a8ea1649949828b32c6f61b
佐藤 匠 <biker-duck.g@ezweb.ne.jp>
2016-04-02 15:33:13 +0900

    ボタンを継承させようと思ったけど、挙動がだいぶ違うので別のオブジェクトとして生成するように変更.タッチしたらパーティクルが出るところとスコア加算、コンボ処理までテスト済み。

--- Signal/Classes/Obj/ColorBlock.cpp
+++ Signal/Classes/Obj/ColorBlock.cpp
@@ -6,23 +6,94 @@
 //
 //
 
-#include "ColorBlockButton.hpp"
-
-
-ColorBlockButton::ColorBlockButton()
-:Button()
+#include "ColorBlock.hpp"
+#include "ScoreMgr.hpp"
+#include "TouchParticle.hpp"
+ColorBlock::ColorBlock()
 {
 
 }
 
-Button* ColorBlockButton::CreateBlockButton(cocos2d::Vec2 pos, cocos2d::Vec2 scale,
-                                            cocos2d::Vec2 ancPos,const char* spritePath,
-                                            ObjectTag tag)
+
+bool ColorBlock::init(cocos2d::Vec2 pos,     cocos2d::Vec2 scale,
+                      Type type,             cocos2d::Vec2 ancPos,
+                      const char* spritePath,ObjectTag     tag)
 {
     
-    auto button = Button::Create(pos,scale,ancPos,spritePath,tag);
+    if(!cocos2d::Sprite::init())
+    {
+        return false;
+    }
     
+    reLifeTimer_ = 0;
+    type_ = Type::None;
     
-    return button;
+    //生成
+    spritePath_ = spritePath;
+    sprite_ = SpriteMgr::Get().Add(spritePath_,Resource::SpriteKey::Block);
+    //ステータス,描画する画像の初期化
+    ColorBlock::InitInfo(pos,scale,ancPos,sprite_,tag);
+//    sprite_->setScale(scale_.x,scale_.y);
+//    sprite_->setPosition(pos_);
+//    sprite_->setAnchorPoint(ancPos_);
+    this->addChild(sprite_);
+    
+    //Updateを登録.
+    this->schedule(schedule_selector(ColorBlock::Update));
+    
+    return true;
+}
+
+
+void ColorBlock::Update(float delta)
+{
+    if(reLifeTimer_ > 0)
+    {
+        
+        reLifeTimer_ -=  1 * delta;
+        if(reLifeTimer_ <= 0){ sprite_->setVisible(true); }
+    }
+    
 
 }
+
+
+bool ColorBlock::onTouchBegan(cocos2d::Touch *touch, cocos2d::Event *event)
+{
+    auto location = touch->getLocation();
+    
+    //ボタン内でカーソルを話したら実行
+    if(Collision::isPointToRect(location,Vec2(pos_.x ,pos_.y),size_,ancPos_))
+    {
+        
+        //壊れてない状態だったらブロックを壊す
+        if(reLifeTimer_ <= 0)
+        {
+            //タッチでブロックをぶっ壊す
+            reLifeTimer_ = reMakeTime_;
+            sprite_->setVisible(false);
+            auto effect = TouchParticle::TouchParticleCreate(pos_,particleScale_,
+                                                             Direction::LeftVec,
+                                                             particletAngle_,
+                                                             particletLifeTime_,particleMax_,
+                                                             particletSpeed_,"button.png");
+            this->addChild(effect,1);
+            
+            ScoreMgr::Get().AddScore();
+        }
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
--- Signal/Classes/Obj/ColorBlock.hpp
+++ Signal/Classes/Obj/ColorBlock.hpp
@@ -7,11 +7,13 @@
 //
 
 #pragma once
-#include "Button.hpp"
+#include "GameObject.hpp"
 
-class ColorBlock : public Button
+class ColorBlock : public GameObject,create_func<ColorBlock>
 {
+    
 public:
+    
     enum Type
     {
         None   = -1,
@@ -23,16 +25,33 @@ public:
         
     };
 
-    ColorBlockButton();
+    ColorBlock();
+    using create_func::Create;
     
-    static Button* CreateBlockButton(cocos2d::Vec2 pos, cocos2d::Vec2 scale,
-                                     cocos2d::Vec2 ancPos     = AncPoint::AnchorCenter,
-                                     const char*   spritePath = "button.png",
-                                     ObjectTag     tag        = ObjectTag::Button);
-    void Update();
+    virtual bool  init(cocos2d::Vec2 pos, cocos2d::Vec2 scale,
+                       Type type,
+                       cocos2d::Vec2 ancPos     = AncPoint::AnchorCenter,
+                       const char*   spritePath = "button.png",
+                       ObjectTag     tag        = ObjectTag::Button);
+    
+    void Update(float delta);
+    
+    //タッチ処理用
+    virtual bool onTouchBegan(cocos2d::Touch* touch, cocos2d::Event* event);
+    virtual void onTouchMoved(cocos2d::Touch* touch, cocos2d::Event* event){};
+    virtual void onTouchEnded(cocos2d::Touch* touch, cocos2d::Event* event){};
     
 private:
     
     Type type_;
+    float reLifeTimer_;
+    const float reMakeTime_ = 0.4f;
+    
+    const cocos2d::Vec2 particleScale_ = Vec2(0.1f,0.1f);
+    const float particletLifeTime_ = 0.45f;
+    const float particletSpeed_ = 200;
+    const int   particletAngle_ = 300;
+    const int   particleMax_ = 20;
+
 
 };
